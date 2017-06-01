0efb24ba4b02450f18cf5f7075e13cd746c00487
Andrey Menangen <menangen@gmail.com>
2017-06-01 22:25:25 +0300

    Vue.js 'poduction' build

--- app/templates/rollup/rollup.config.js
+++ app/templates/rollup/rollup.config.js
@@ -1,4 +1,6 @@
-<% if (javascript === 'vue' || javascript === 'preact') { %>const nodeResolve = require("rollup-plugin-node-resolve");<% if (javascript === 'vue') { %>const vue = require("rollup-plugin-vue");<% } %>
+<% if (javascript === 'vue' || javascript === 'preact') { %>const nodeResolve = require("rollup-plugin-node-resolve");
+<% if (javascript === 'vue') { %>const vue = require("rollup-plugin-vue");
+const replace = require('rollup-plugin-replace');<% } %>
 <% if (javascript === 'preact') { %>const babel = require("rollup-plugin-babel");<% }} %>
 export default {
     entry: 'src/javascript/main.js',
@@ -7,7 +9,10 @@ export default {
     moduleName: '<%= projectName %>',
     dest: 'dist/js/bundle.js'<% if (javascript === 'vue' || javascript === 'preact') { %>,
     plugins: [<% if (javascript === 'vue') { %>
-        vue({compileTemplate: true}),<% } %><% if (javascript === 'preact') { %>
+        vue({compileTemplate: true}),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),<% } %><% if (javascript === 'preact') { %>
         babel({
             plugins: [
                 ["transform-react-jsx", { "pragma": "h" }]// default pragma is preact.h
