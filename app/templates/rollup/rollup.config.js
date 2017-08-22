ed343eeba7a8717b5ed427a3c012eb7d7112e9e7
menangen <menangen@gmail.com>
2017-08-22 14:43:12 +0300

    Rollup umd => iife

--- app/templates/rollup/rollup.config.js
+++ app/templates/rollup/rollup.config.js
@@ -1,22 +1,22 @@
 <% if (javascript === 'vue' || javascript === 'preact') { %>const nodeResolve = require("rollup-plugin-node-resolve");
 <% if (javascript === 'vue') { %>const vue = require("rollup-plugin-vue");
-const replace = require('rollup-plugin-replace');<% } %>
-<% if (javascript === 'preact') { %>const babel = require("rollup-plugin-babel");<% }} %>
+const replace = require('rollup-plugin-replace');<% } %><% if (javascript === 'preact') { %>const babel = require("rollup-plugin-babel");<% }} %>
 const production = process.env.NODE_ENV === "production";
+
 export default {
-    entry: 'src/javascript/main.js',
-    format: 'umd',
+    entry: "src/javascript/main.js",
+    format: "iife",
     useStrict: false,
-    moduleName: '<%= projectName %>',
+    moduleName: "<%= projectName %>",
     sourceMap: !production,
-    dest: 'dist/js/bundle.js'<% if (javascript === 'vue' || javascript === 'preact') { %>,
-    plugins: [<% if (javascript === 'vue') { %>
+    dest: "dist/js/bundle.js"<% if (javascript === "vue" || javascript === "preact") { %>,
+    plugins: [<% if (javascript === "vue") { %>
         vue({compileTemplate: true}),
         replace({
-            'process.env.NODE_ENV': JSON.stringify('development')// or "production"
-        }),<% } %><% if (javascript === 'preact') { %>
+            "process.env.NODE_ENV": JSON.stringify("development")// or "production"
+        }),<% } %><% if (javascript === "preact") { %>
         babel({
-            exclude: 'node_modules/**',
+            exclude: "node_modules/**",
             plugins: [
                 ["transform-react-jsx", { "pragma": "h" }]// default pragma is preact.h
             ]}),<% } %>
